a5cba8c287eef4fc1b91fd0578c4e07808ee2dcf
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-03-06 15:02:28 -0500

    Fix font sizes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,35 @@
-import { Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+import { styled } from '@mui/material/styles';
 
 const interests = ["React", "Material Design", "Blockchain"]
 
+const StyledBlockquote = styled('blockquote')({
+  borderLeft: "5px solid #795548",
+  color: "#795548",
+  margin: "20px 0",
+  paddingLeft: "1.5rem"
+});
+const StyledSpan = styled('span')(({ theme }) => ({
+  fontSize: theme.typography.h6.fontSize
+}));
+
 export default function About() {
   return (
-    <>
-      <blockquote>
+    <Container>
+      <StyledBlockquote>
         <Typography variant="h4">Mission-driven full stack developer with a passion for thoughtful architecture, collaboration, and teaching</Typography>
-      </blockquote>
-      <Typography variant="body2" gutterBottom>
+      </StyledBlockquote>
+      <Typography variant="h6" gutterBottom>
         I've always sought out opportunities and challenges that are meaningful to me. Although my professional path has taken many twists and turns - from touring and recording artist, to employee of the year at a non-protfit - I've never stopped engaging my passion to help others and solve problems.
       </Typography>
-      <Typography variant="body2" gutterBottom>
+      <Typography variant="h6" gutterBottom>
         As a web developer, I enjoy my obsessive attention to detail, my unequivocal love for making things, and my mission driven work ethic to change the world. That's why I'm excitedd to make a big impact at high growth companies.
       </Typography>
       <Typography>
         {interests.map((val, idx) => {
-          return <span key={`interests_${idx}`}>{(idx ? ', ' : '') + val}</span>
+          return <StyledSpan key={`interests_${idx}`}>{(idx ? ' // ' : '') + val}</StyledSpan>
         })}
       </Typography>
-    </>
+    </Container>
   )
 }
