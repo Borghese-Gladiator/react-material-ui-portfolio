fff421995dff004e404799b9b8e3843c8077d0b8
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-03-06 15:19:33 -0500

    Refactor landing names and add opacity

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,36 +1,45 @@
 import { Typography } from "@mui/material";
 import { styled } from '@mui/material/styles';
 
-const SectionWithBackground = styled('section')({
+const Section = styled('section')({
   height: "100vh",
   backgroundImage: "url(/profile.jpg)",
   backgroundSize: "cover",
   backgroundRepeat: "no-repeat",
-  backgroundPosition: "center center"
+  backgroundPosition: "center center",
+  position: "relative"
 });
-const AbsoluteDiv = styled('div')({
+const DivAbsolute = styled('div')({
+  top: 0,
+  left: 0,
   position: "absolute"
 });
-const ContrastTypography = styled(Typography)(({ theme }) => ({
+const DivOverlay = styled('div')({
+  backgroundColor: "rgba(0, 0, 0, 0.4)",
+  height: '100%'
+})
+const StyledTypography = styled(Typography)(({ theme }) => ({
   color: theme.palette.primary.contrastText,
+  paddingTop: theme.spacing(4),
 }));
-const UnderlineSpan = styled('span')(({ theme }) => ({
+const UnderlineText = styled('span')(({ theme }) => ({
   textDecoration: `underline ${theme.palette.primary.main} `
 }));
-const BorderSpan = styled('span')(({ theme }) => ({
+const BorderText = styled('span')(({ theme }) => ({
   backgroundColor: theme.palette.primary.main,
   color: theme.palette.primary.contrastText,
 }));
 
 export default function Landing() {
   return (
-    <SectionWithBackground>
-      <AbsoluteDiv>
-        <ContrastTypography variant="h4" ml={3} mt={6}>
-          I want to <BorderSpan>make things</BorderSpan> <br />
-          that <UnderlineSpan>make a difference</UnderlineSpan>
-        </ContrastTypography>
-      </AbsoluteDiv>
-    </SectionWithBackground>
+    <Section>
+      <DivOverlay />
+      <DivAbsolute>
+        <StyledTypography variant="h3" ml={3} mt={6}>
+          I want to <BorderText>make things</BorderText> <br />
+          that <UnderlineText>make a difference</UnderlineText>
+        </StyledTypography>
+      </DivAbsolute>
+    </Section>
   )
 }
