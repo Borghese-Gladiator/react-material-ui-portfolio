3f7fddbb3aadca7fae4bf74f429c24a3c14f6558
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-03-21 00:05:34 -0400

    Create contact and education sections

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,8 +1,57 @@
-import { Typography } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import { Box, Link, Avatar, Typography } from "@mui/material";
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing(3),
+  marginLeft: theme.spacing(8),
+  marginRight: theme.spacing(8)
+}));
+const Row = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+}));
+const StyledAvatar = styled(Avatar)(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.main,
+  width: 56,
+  height: 56
+}));
+
+const mailLink = `href="mailto:tim.shee0791@gmail.com"`;
+const githubLink = "https://github.com/Borghese-Gladiator";
+const linkedinLink = "https://www.linkedin.com/in/timothy-shee";
 
 export default function Contact() {
   return (
-    <>
-    </>
+    <StyledBox>
+      <Link href={mailLink}>
+        <Row>
+          <StyledAvatar>
+            <EmailIcon />
+          </StyledAvatar>
+          <Typography ml={2} variant="subtitle1" color="primary">tim.shee0791@gmail.com</Typography>
+        </Row>
+      </Link>
+      <Link href={githubLink}>
+        <Row>
+          <StyledAvatar>
+            <GitHubIcon />
+          </StyledAvatar>
+          <Typography ml={2} variant="subtitle1" color="primary">github.com/borghese-gladiator</Typography>
+        </Row>
+      </Link>
+      <Link href={linkedinLink}>
+        <Row>
+          <StyledAvatar>
+            <LinkedInIcon />
+          </StyledAvatar>
+          <Typography ml={2} variant="subtitle1" color="primary">github.com/borghese-gladiator</Typography>
+        </Row>
+      </Link>
+    </StyledBox>
   )
 }
--- src/components/sections/Education.jsx
+++ src/components/sections/Education.jsx
@@ -1,8 +1,30 @@
-import { Typography } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import { Box, Grid, Card, Typography } from "@mui/material";
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  gap: theme.spacing(3),
+  marginLeft: theme.spacing(8),
+  marginRight: theme.spacing(8)
+}));
+const StyledCard = styled(Card)(({ theme }) => ({
+  padding: theme.spacing(3)
+}));
 
 export default function Education() {
   return (
-    <>
-    </>
+    <StyledBox>
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <StyledCard>
+            <Typography variant="h5" color="primary">UMASS AMHERST</Typography>
+            <Typography variant="subtitle1" color="secondary">Amherst, MA</Typography>
+            <ul>
+              <li>Bachelor's in Computer Science</li>
+            </ul>
+          </StyledCard>
+        </Grid>
+      </Grid>
+    </StyledBox>
   )
 }
